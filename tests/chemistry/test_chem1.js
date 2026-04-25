48650aee01ee9dee425d01e9faa770f085fbc061
SSH <149168194+SSHH250@users.noreply.github.com>
2026-04-25 23:14:29 +0300

    Add files via upload

---
+++ tests/chemistry/test_chem1.js
@@ -0,0 +1,27 @@
+{
+    "id": "test_chem_1",
+    "name": "Основы органической химии",
+    "points": 100,
+    "numberOfQuestions": 3,
+    "questionBanks": [
+        {
+            "topic": "Алканы",
+            "questions": [
+                {
+                    "type": "choice",
+                    "text": "Какая формула у метана?",
+                    "options": ["CH₄", "C₂H₆", "C₃H₈", "C₄H₁₀"],
+                    "correct": 0,
+                    "explanation": "Метан — CH₄"
+                },
+                {
+                    "type": "multiple-choice",
+                    "text": "Какие цвета есть в радуге?",
+                    "options": ["Красный", "Коричневый", "Серый", "Синий"],
+                    "correct": [0, 3],
+                    "explanation": "В радуге есть красный и синий (голубой) цвета"
+                }
+            ]
+        }
+    ]
+}
